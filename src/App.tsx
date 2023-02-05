d8fdf5f917a37f2a4c9a67644a9534dc3dafa465
Jinny <ppwm111@naver.com>
2023-02-05 15:38:43 +0900

    Feature/2 typescript setting (#3)
    * npx create-react-app
    
    * eslint/prttier 설정 #2

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import Test from './components/Test';
+
+const App: FC = () => {
+  return (
+    <div>
+      초기 세팅
+      <Test />
+    </div>
+  );
+};
+
+export default App;
