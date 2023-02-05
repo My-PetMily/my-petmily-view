1e25ab848f3f8b5cbe6c0f8f5c0e1d0f871630ce
Jinny <ppwm111@naver.com>
2023-02-05 15:34:45 +0900

    eslint/prttier 설정 #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import React, { FC } from 'react';
 import Test from './components/Test';
 
-function App() {
+const App: FC = () => {
   return (
     <div>
       초기 세팅
       <Test />
     </div>
   );
-}
+};
 
 export default App;
