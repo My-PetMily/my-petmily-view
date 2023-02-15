d883d0c3d45c852e98cfad1783d960b7eb5634f2
Jinny <ppwm111@naver.com>
2023-02-15 16:33:20 +0900

    feat: 최대한 비슷하게 완료 #4

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Container,
   Contents,
-  RegisterInput,
   RegisterTitle,
   Wrap,
+  RegisterTable,
+  TableTh,
+  Input,
+  Btn,
+  WrapEmail,
+  SubmitWrap,
+  SubmitBtn,
 } from './styles';
 
 const Register = () => {
+  const [selectMember, setSelectMember] = useState<{ [x: string]: string }>({
+    select: 'basic',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setSelectMember({
+      [name]: value,
+    });
+
+    console.log(selectMember);
+  };
+
   return (
     <Wrap>
       <header>이미지</header>
@@ -17,64 +36,92 @@ const Register = () => {
             <h2 style={{ margin: '0 auto' }}>REGISTER</h2>
           </RegisterTitle>
           <form>
-            <table border={1}>
-              <tr>
-                <th>회원구분</th>
-                <td>
-                  <RegisterInput type="radio" checked />
-                  <label>개인회원</label>
-                </td>
-                <td>
-                  <RegisterInput type="radio" />
-                  <label>사업자회원</label>
-                </td>
-              </tr>
-            </table>
-            <h3>기본정보</h3>
-            <table>
-              <tr>
-                <th>닉네임</th>
-                <td>
-                  <RegisterInput type="text" />
-                  <span>닉네임을 입력해주세요</span>(한글/영대소문자 최대 20자)
-                </td>
-              </tr>
-              <tr>
-                <th>비밀번호</th>
-                <td>
-                  <RegisterInput type="password" />
-                  <span>
-                    (영문 대소문자/숫자/특수문자 중 2가지 이상 조합, 10자~16자)
-                  </span>
-                </td>
-              </tr>
-              <tr>
-                <th>비밀번호 확인</th>
-                <td>
-                  <RegisterInput type="password" />
-                  <span>에러메시지 띄우는 곳</span>
-                </td>
-              </tr>
-              <tr>
-                <th>이메일</th>
-                <td>
-                  <RegisterInput type="email" />
-                  <span>에러메시지 띄우는 곳</span>
-                  <button>인증번호 받기</button>
-                </td>
-              </tr>
-              <tr>
-                <th>이메일 확인</th>
-                <td>
-                  <RegisterInput type="text" />
-                  <span>에러메시지 띄우는 곳</span>
-                  <button>확인</button>
-                </td>
-              </tr>
-            </table>
-            <div>
-              <button>회원가입</button>
-            </div>
+            <RegisterTable>
+              <tbody>
+                <tr>
+                  <TableTh>회원구분</TableTh>
+                  <td>
+                    <Input
+                      type="radio"
+                      name="selectMember"
+                      value="basic"
+                      onChange={handleChange}
+                      defaultChecked
+                    />
+                    <label style={{ marginRight: 5 }}>개인회원</label>
+                    <Input
+                      type="radio"
+                      name="selectMember"
+                      value="company"
+                      onChange={handleChange}
+                    />
+                    <label>사업자회원</label>
+                  </td>
+                </tr>
+              </tbody>
+            </RegisterTable>
+            <h3
+              style={{
+                margin: '50px 0 20px',
+                fontSize: 12,
+                color: '#353535',
+                fontWeight: 'Bold',
+              }}
+            >
+              기본정보
+            </h3>
+            <RegisterTable>
+              <tbody>
+                <tr>
+                  <TableTh>닉네임</TableTh>
+                  <td>
+                    <Input type="text" />
+                    <span>닉네임을 입력해주세요</span>(한글/영대소문자 최대
+                    20자)
+                  </td>
+                </tr>
+                <tr>
+                  <TableTh>비밀번호</TableTh>
+                  <td>
+                    <Input type="password" />
+                    <span>
+                      (영문 대소문자/숫자/특수문자 중 2가지 이상 조합,
+                      10자~16자)
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <TableTh>비밀번호 확인</TableTh>
+                  <td>
+                    <Input type="password" />
+                    <span></span>
+                  </td>
+                </tr>
+                <tr>
+                  <TableTh>이메일</TableTh>
+                  <td>
+                    <WrapEmail>
+                      <Input type="email" />
+                      <span></span>
+                      <Btn>인증번호 받기</Btn>
+                    </WrapEmail>
+                  </td>
+                </tr>
+                <tr>
+                  <TableTh>이메일 확인</TableTh>
+                  <td>
+                    <WrapEmail>
+                      <Input type="text" />
+                      <span></span>
+                      <Btn>확인</Btn>
+                    </WrapEmail>
+                  </td>
+                </tr>
+              </tbody>
+            </RegisterTable>
+            <SubmitWrap>
+              <SubmitBtn>회원가입</SubmitBtn>
+            </SubmitWrap>
           </form>
         </Contents>
       </Container>
