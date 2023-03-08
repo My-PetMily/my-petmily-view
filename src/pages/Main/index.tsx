dfea26def0b013d42e5c854e5337795403f7fa39
Jinny <ppwm111@naver.com>
2023-03-08 23:02:41 +0900

    feat: 3가지 API 배포 테스트

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -7,9 +7,24 @@ const MainPage = () => {
       withCredentials: true,
     });
   };
+
+  const api2 = () => {
+    axios.get('http://127.0.0.1:8081/email?email=ppwm111@naver.com', {
+      withCredentials: true,
+    });
+  };
+
+  const api3 = () => {
+    axios.get('http://sujeong0.cafe24.com:8081/email?email=ppwm111@naver.com', {
+      withCredentials: true,
+    });
+  };
+
   return (
     <div>
-      <button onClick={api}>API버튼</button>
+      <button onClick={api}>package.json proxy API버튼</button>
+      <button onClick={api2}>no Proxy 127.0.0.1 API버튼</button>
+      <button onClick={api3}>domain API버튼</button>
     </div>
   );
 };
