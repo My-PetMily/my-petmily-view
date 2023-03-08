498cf7babb1667b2a9a25cf403edd69d2f49fbf5
Jinny <ppwm111@naver.com>
2023-03-08 22:56:37 +0900

    feat: 배포 후 API 테스트

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,17 @@
+import axios from 'axios';
 import React from 'react';
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  const api = () => {
+    axios.get('/email?email=ppwm111@naver.com', {
+      withCredentials: true,
+    });
+  };
+  return (
+    <div>
+      <button onClick={api}>API버튼</button>
+    </div>
+  );
 };
 
 export default MainPage;
